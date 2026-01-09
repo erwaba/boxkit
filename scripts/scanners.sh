986f4c2879cc2ff7e795a631767ca8f2e6e9b7ef
erwaba <183932306+erwaba@users.noreply.github.com>
2026-01-08 19:00:29 -0800

    Add .desktop file for g2scan2pdf.

--- scripts/scanners.sh
+++ scripts/scanners.sh
@@ -6,3 +6,22 @@
 # Update the container and install packages
 dnf update -y
 grep -v '^#' ./scanners.packages | xargs dnf install -y
+
+# create .desktop file for g2scan2pdf, so that distrobox assemble can detect it
+mkdir -p /usr/share/applications
+
+cat > /usr/share/applications/gscan2pdf.desktop << 'EOF'
+[Desktop Entry]
+Name=gscan2pdf
+GenericName=PDF Scanner
+Comment=Scan documents and create PDF files
+Exec=gscan2pdf %F
+Icon=gscan2pdf
+Terminal=false
+Type=Application
+Categories=Graphics;Scanning;Office;
+MimeType=image/png;image/jpeg;image/tiff;application/pdf;
+StartupNotify=true
+EOF
+
+chmod 644 /usr/share/applications/gscan2pdf.desktop
