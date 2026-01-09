07412b4fd16336bb40d07ac158c5ed49a3aaa474
erwaba <183932306+erwaba@users.noreply.github.com>
2026-01-09 10:28:05 -0800

    Revise to have empty package list for blabels.

--- scripts/blabels.sh
+++ scripts/blabels.sh
@@ -5,5 +5,4 @@
 
 # Update the container and install packages
 dnf update -y
-#nothing to install
-#grep -v '^#' ./blabels.packages | xargs dnf install -y
+grep -v '^#' ./blabels.packages | xargs dnf install -y
